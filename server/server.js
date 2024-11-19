516f16b36716917e8da401088cda3d8b1621b384
Arshdeep Singh <imarshdeep99@gmail.com>
2024-11-19 22:34:35 +0530

    update backend

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ app.use("/api/product", productRouter)
 app.use("/api/user", userRouter)
 app.use("/api/cart", cartRouter)
 app.use("/api/order", orderRouter)
-app.use("/images", express.static('uploads'))
+// app.use("/images", express.static('uploads'))
 
 app.get("/",(req,res)=>{
   res.send("Api Working")
